b5019a15ed52a4d87f42ddf4b4462db9cf5a8cce
FabienTreve <trevefabien@gmail.com>
2020-03-19 13:12:07 +0100

    Init git

---
+++ routes/ideas.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const router = express.Router();
+const {ensureAuthenticated} = require('../helpers/auth');
+
+// Load IdeaModel
+require('../models/Idea');
+const Idea = mongoose.model('ideas');
+
+// Idea Index Page
+router.get('/', ensureAuthenticated, (req, res) => {
+    Idea.find({})
+        .sort({date: 'desc'})
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas: ideas,
+            });
+        });    
+}); 
+
+// Add Idea Form
+router.get('/add', ensureAuthenticated, (req, res) => {
+    res.render('ideas/add');
+}); 
+
+// Edit Idea Form
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+    .then(idea => {
+        res.render('ideas/edit', {
+            idea: idea
+        });
+    })
+}); 
+
+// Process Form
+router.post('', ensureAuthenticated, (req, res) => {
+    let errors = [];
+
+    //Server-side validation (viens completer l'attribut input "required" coté client)
+    if(!req.body.title){
+        errors.push({text:'Please add a title'});
+    }
+    if(!req.body.details){
+        errors.push({text:'Please add some details'});
+    }
+
+    if(errors.length > 0){
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details,
+        })
+    } else {
+        const NewUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new Idea(NewUser)
+            .save()
+            .then(idea => {
+                req.flash('success_msg', 'Videa Idea added');
+                res.redirect('/ideas');
+            })
+    }
+}); 
+
+// Edit Form process
+router.put('/:id', ensureAuthenticated, (req, res) => {
+    Idea.findOne({
+        _id : req.params.id
+    })
+    .then(idea => {
+        // New value
+        idea.title = req.body.title;
+        idea.details = req.body.details;
+
+        idea.save()
+            .then(idea =>{
+                res.redirect('/ideas');
+            })
+    })
+});
+
+//Delete Idea
+router.delete('/:id', ensureAuthenticated, (req, res) => {
+    Idea.remove({_id: req.params.id})
+    .then(() => {
+        req.flash('success_msg', 'Videa Idea removed');
+        res.redirect('/ideas');
+    })
+});
+
+module.exports = router;
