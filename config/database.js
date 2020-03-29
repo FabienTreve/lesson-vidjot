7e540cec8c46e27e2bbbc078eb1d259c2c44b924
FabienTreve <trevefabien@gmail.com>
2020-03-29 12:35:03 +0200

    fix deploy new password

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === "production"){
-    module.exports = {mongoURI : 'mongodb+srv://FabienT:623ukJKRGtGAewYW@clusterftr-wrdqy.mongodb.net/test?retryWrites=true&w=majority'}
+    module.exports = {mongoURI : 'mongodb+srv://FabienT:TODtNi8rldnwQ6x9@clusterftr-wrdqy.mongodb.net/test?retryWrites=true&w=majority'}
 } else {
     //module.exports = {mongoURI : 'mongodb://localhost/vidjot-dev'}
-    module.exports = {mongoURI : 'mongodb+srv://FabienT:623ukJKRGtGAewYW@clusterftr-wrdqy.mongodb.net/test?retryWrites=true&w=majority'}
+    module.exports = {mongoURI : 'mongodb+srv://FabienT:TODtNi8rldnwQ6x9@clusterftr-wrdqy.mongodb.net/test?retryWrites=true&w=majority'}
 }
